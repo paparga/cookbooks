d2b1f90fdd95daa06de8ed5be649ad721312b488
Snuggs <rashaunstovall@gmail.com>
2015-03-31 18:52:48 -0400

    [chore] Override AWS OpsWorks Node JS configuration

--- opsworks_nodejs/recipes/create_env_file.rb
+++ opsworks_nodejs/recipes/create_env_file.rb
@@ -1,3 +1,4 @@
+message "\n\n\nStarting Node\n\n"
 node[:deploy].each do |application, deploy|
   template File.join(deploy[:deploy_to], "shared","app.env") do
     source "app.env.erb"
@@ -8,7 +9,12 @@ node[:deploy].each do |application, deploy|
       :environment => OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables].merge({FOO: 'BAR'}))
     )
 
+    message "\n\n\nDEPLOY: #{deploy.inspect}"
     Chef::Log.debug("\n\n\nDEPLOY: #{deploy.inspect}")
+
+
+    message "\n\n\nEnvironment: #{OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables].merge({FOO: 'BAR'}))}"
+
     Chef::Log.debug("\n\n\nEnvironment: #{OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables].merge({FOO: 'BAR'}))}")
 
     only_if {File.exists?("#{deploy[:deploy_to]}/shared")}
