8909b31a1f9ff504b2a1cae56ba2ddfabca16c31
Snuggs <rashaunstovall@gmail.com>
2015-04-01 16:21:15 -0400

    Remove deploy scripts

--- airpair_environment/recipes/default.rb
+++
@@ -1,6 +0,0 @@
-node[:deploy].each do |application, deploy|
-  pathname = File.join(deploy[:deploy_to], "shared","app.env")
-  File.open(pathname, 'a') do |f|
-    f.puts 'export AIRPAIR_CLI=ROCKS'
-  end
-end
