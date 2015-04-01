a4d0d08110e0e7390f586220649fda8ec6da6f56
Snuggs <rashaunstovall@gmail.com>
2015-03-31 20:24:06 -0400

    Add GetHuman cookbooks

---
+++ gethuman/metadata.rb
@@ -0,0 +1,12 @@
+name        "opsworks_nodejs"
+description 'Installs and configures a Node.js application server & UPDATES ENVIRONMENT VARIABLES'
+maintainer  "Get Human"
+license     "Apache 2.0"
+version     "1.0.0"
+
+# depends 'deploy'
+# depends 'opsworks_commons'
+
+message "\n\n\nGETHUMAN METADATA:"
+Chef::Log.debug("\n\n\n(Chef) GETHUMAN METADATA:")
+
