0facadf0225f6cc55550ed7de0ab10b19dcbb205
Snuggs <rashaunstovall@gmail.com>
2015-03-31 20:27:06 -0400

    Add GetHuman cookbooks

--- gethuman/recipes/default.rb
+++ gethuman/recipes/default.rb
@@ -1,41 +1,39 @@
-include_recipe 'deploy'
+message "\n\n\nGETHUMAN RECIPE DEFAULT:"
+Chef::Log.debug("\n\n\n(Chef) GETHUMAN RECIPE DEFAULT:")
 
 node[:deploy].each do |application, deploy|
-  if deploy[:application_type] != 'nodejs'
-    Chef::Log.debug("Skipping deploy::nodejs for application #{application} as it is not a node.js app")
-    next
-  end
+message "\n\n\nNODE DEPLOY: #{deploy.inspect}"
+Chef::Log.debug("\n\n\n(CHEF)NODE DEPLOY: #{deploy.inspect}")
 
-  opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
-    path deploy[:deploy_to]
-  end
+# opsworks_deploy_dir do
+#   user deploy[:user]
+#   group deploy[:group]
+#   path deploy[:deploy_to]
+# end
 
-  opsworks_deploy do
-    deploy_data deploy
-    app application
-  end
+# opsworks_deploy do
+#   deploy_data deploy
+#   app application
+# end
 
-  opsworks_nodejs do
-    deploy_data deploy
-    app application
-  end
+# opsworks_nodejs do
+#   deploy_data deploy
+#   app application
+# end
 
-  application_environment_file do
-    user deploy[:user]
-    group deploy[:group]
-    path ::File.join(deploy[:deploy_to], "shared")
-    message "\n\n\nAPPLICATION Environment FILE: #{deploy[:environment_variables].merge({FOO: 'BAR'})}"
-    environment_variables deploy[:environment_variables]
-  end
+# application_environment_file do
+#   user deploy[:user]
+#   group deploy[:group]
+#   path ::File.join(deploy[:deploy_to], "shared")
+#   message "\n\n\nAPPLICATION Environment FILE: #{deploy[:environment_variables].merge({FOO: 'BAR'})}"
+#   environment_variables deploy[:environment_variables]
+# end
 
-  ruby_block "restart node.js application #{application}" do
-    block do
-      Chef::Log.info("restart node.js via: #{node[:deploy][application][:nodejs][:restart_command]}")
-      Chef::Log.info(`#{node[:deploy][application][:nodejs][:restart_command]}`)
-      $? == 0
-    end
-  end
+# ruby_block "restart node.js application #{application}" do
+#   block do
+#     Chef::Log.info("restart node.js via: #{node[:deploy][application][:nodejs][:restart_command]}")
+#     Chef::Log.info(`#{node[:deploy][application][:nodejs][:restart_command]}`)
+#     $? == 0
+#   end
+# end
 end
-
