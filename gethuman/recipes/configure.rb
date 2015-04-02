6f67acc2f60d192def761dc6aef7c9b490c8ec3c
Snuggs <rashaunstovall@gmail.com>
2015-04-02 12:24:30 -0400

    Refactor configuration into separate recipe

---
+++ gethuman/recipes/configure.rb
@@ -0,0 +1,25 @@
+Chef::Log.warn "\n\n\n CONFIGURE\n NODE ENV VARIAbLES: #{node[:environment_variables]}"
+
+node[:deploy].each do |application, deploy|
+
+  Chef::Log.warn "\n\n\nNormal: #{deploy[:environment_variables]}"
+
+  application_environment_file do
+    user deploy[:user]
+    group deploy[:group]
+
+    path ::File.join(deploy[:deploy_to], 'shared')
+    environment_variables merged_environment(deploy[:environment_variables])
+  end
+end
+
+def merged_environment(variables)
+  Chef::Log.warn "app.env: #{variables.merge(node_environment_variables)}"
+
+  variables.merge node_envirionment_variables
+end
+
+def node_environment_variables
+  node[:environment_variables]
+end
+
--- gethuman/recipes/default.rb
+++ gethuman/recipes/default.rb
@@ -1,20 +1,3 @@
-Chef::Log.warn "\n\n\n NODE ENV VARIAbLES: #{node[:environment_variables]}"
+Chef::Log.warn "\n\n\n Running gethuman::default"
 
-node[:deploy].each do |application, deploy|
-
-  Chef::Log.warn "\n\n\nNormal: #{deploy[:environment_variables]}"
-
-  application_environment_file do
-    user deploy[:user]
-    group deploy[:group]
-
-    path ::File.join(deploy[:deploy_to], 'shared')
-    environment_variables merged_environment(deploy[:environment_variables])
-  end
-end
-
-def merged_environment(variables)
-  Chef::Log.warn "\n\n\nAPPLICATION Environment FILE: #{variables.merge(node[:environment_variables])}"
-
-  variables.merge node[:environment_variables]
-end
+include_recipe "gethuman::configure"
