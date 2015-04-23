daf0cd7216073a07f07b612341467c372b9014ae
Snuggs <rashaunstovall@gmail.com>
2015-04-23 18:17:34 -0400

    Update pm2 dependency

--- pm2/libraries/helpers.rb
+++ pm2/libraries/helpers.rb
@@ -87,13 +87,15 @@ end
 
 def pm2_start_or_restart_app(filename)
   execute "pm2 startOrRestart #{filename}" do
-    command "/usr/local/bin/pm2 startOrRestart #{filename} && ls -al /etc/pm2/conf.d/ && /usr/local/bin/pm2 list"
+    command "sudo /usr/local/bin/pm2 startOrRestart #{filename} && sudo ls -al /etc/pm2/conf.d/ && sudo /usr/local/bin/pm2 list"
     command "ls -al /etc/pm2/conf.d/"
     command "pm2 list"
 
     Chef::Log.warn(`ls -al /etc/pm2/conf.d/`)
     Chef::Log.warn(`/usr/local/bin/pm2 list`)
     Chef::Log.warn(`cat /etc/pm2/conf.d/server.json`)
+    Chef::Log.warn(`sudo /usr/local/bin/pm2 startOrRestart #{filename} && sudo ls -al /etc/pm2/conf.d/ && sudo /usr/local/bin/pm2 list`)
+    Chef::Log.warn(`echo "$USER"`)
   end
 end
 
