f624cc83470c48300da371929cf3b721b9e835f3
Snuggs <rashaunstovall@gmail.com>
2015-04-23 13:17:43 -0400

    Remove deployment check in default cookbook

--- gethuman/recipes/default.rb
+++ gethuman/recipes/default.rb
@@ -2,13 +2,9 @@ Chef::Log.warn "\n\n\n Running gethuman::default"
 
 package 'nginx'
 
-if node['opsworks']['instance']['layers']
-  .include?("#{deploy['application_type']}-app")
-
-  include_recipe 'gethuman::ssl'
-  include_recipe 'gethuman::nginx'
-  include_recipe 'gethuman::caching'
-  include_recipe 'gethuman::nodejs_start'
-  include_recipe 'gethuman::nodejs_stop'
-  include_recipe 'gethuman::environment'
-end
+include_recipe 'gethuman::ssl'
+include_recipe 'gethuman::nginx'
+include_recipe 'gethuman::caching'
+include_recipe 'gethuman::nodejs_start'
+include_recipe 'gethuman::nodejs_stop'
+include_recipe 'gethuman::environment'
