8ab64babd53f71d175ae20c0286f189134110f4c
Jeff Whelpley <jeff@gethuman.com>
2015-05-05 16:13:22 -0400

    Trying both nodejs and nodejs_from_binary again

--- nodejs-wrapper/attributes/default.rb
+++ nodejs-wrapper/attributes/default.rb
@@ -1,7 +1,7 @@
 
 # Override node the version
 default['nodejs']['version'] = '0.12.2'
-default['nodejs']['install_method'] = 'binary'
+# default['nodejs']['install_method'] = 'binary'
 
 default["nodebin"]["location"] = '/usr/bin/node'
 default["nodebin"]["opsworks_location"] = '/usr/local/bin/node'
--- nodejs-wrapper/recipes/default.rb
+++ nodejs-wrapper/recipes/default.rb
@@ -8,3 +8,4 @@
 #
 
 include_recipe 'nodejs'
+include_recipe 'nodejs_from_binary'
