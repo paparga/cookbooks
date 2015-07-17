0f2bae0d0e42d002f3632a54aa8ee47d90927f3f
Will Button <will.button@traxtech.com>
2015-07-16 20:51:52 -0700

    Oops. That was a fail

--- logs/attributes/default.rb
+++ logs/attributes/default.rb
@@ -1,11 +1,1 @@
 default[:cwlogs][:logfile] = '/srv/www/app/current/log/out.log'
-
-include_recipe "logrotate"
-
-logrotate_app "newrelic" do
-  cookbook  "logrotate"
-  path      "/srv/www/app/current/newrelic_agent.log"
-  options   ["copytruncate", "missingok", "notifempty"],
-  frequency "daily"
-  rotate    7
-end
