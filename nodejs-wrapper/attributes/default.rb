b077d258a123d3b9435c786cb3630a4701587748
Jeff Whelpley <jeff@gethuman.com>
2015-09-17 21:51:01 -0400

    Upgrading node to 4.1.0

--- nodejs-wrapper/attributes/default.rb
+++ nodejs-wrapper/attributes/default.rb
@@ -1,7 +1,7 @@
 
 # Override node the version
 default['nodejs']['engine'] = 'node'
-default['nodejs']['version'] = '4.0.0'
+default['nodejs']['version'] = '4.1.0'
 # default['nodejs']['install_method'] = 'binary'
 
 default["nodebin"]["location"] = '/usr/bin/node'
